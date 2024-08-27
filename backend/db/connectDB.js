546463cff706c519ebfe9fe3c10f7cca8d44991a
PriyanshuValiya <147643182+PriyanshuValiya@users.noreply.github.com>
2024-08-27 14:02:32 +0530

    Project File Upload

--- backend/db/connectDB.js
+++ backend/db/connectDB.js
@@ -5,7 +5,7 @@ dotenv.config();
 
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect("mongodb+srv://valiyapriyansukumar:priyansu2006@cluster0.mrbmo.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0");
+    const conn = await mongoose.connect("mongodb+srv://valiyapriyansukumar:priyansu2006@cluster0.mrbmo.mongodb.net/threads?retryWrites=true&w=majority&appName=Cluster0");
     console.log("MongoDB Connection Successfull !!");
   } catch (err) {
     console.error(`Error : ${err.message}`);
